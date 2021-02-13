4ee468dec7cb081e6d3af46cf336339531871a84
amar00k <danielneves2005@gmail.com>
2021-02-13 05:23:02 +0000

    wip: test updateInputLayoutItem

--- tests/testthat/test-blueprint.R
+++ tests/testthat/test-blueprint.R
@@ -25,3 +25,24 @@ test_that("traverseInputLayout works", {
   test.layout <- traverseInputLayout(layout, function(x) { x$name <- "test"; x }, flatten = TRUE)
   expect_true(all(sapply(test.layout, "[[", "name") == "test"))
 })
+
+
+test_that("updateInputLayoutItem works", {
+  layout <- blueprint$input.layout
+
+  new.input <- slateInput("x", "character", "")
+  new.group <- inputGroup("group")
+  new.page <- inputPage("page")
+
+  new.layout <- updateInputLayoutItem(layout, new.page)
+  expect_true(length(new.layout$pages) == 4)
+  expect_true(names(new.layout$pages)[4] == "page")
+
+  new.layout <- updateInputLayoutItem(layout, new.group, c("Graphical parameters"))
+  expect_true(length(new.layout$pages$`Graphical parameters`$groups) == 2)
+
+  new.layout <- updateInputLayoutItem(layout, new.input, c("Graphical parameters"))
+})
+
+
+
