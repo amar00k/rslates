04619e0217e0cbf7af5b58a9f9f8b235464ec19e
amar00k <danielneves2005@gmail.com>
2021-02-17 11:02:16 +0000

    enh: first custom theme: Natural (soft light).

--- R/theme.R
+++ R/theme.R
@@ -4,12 +4,11 @@
 
 rslate.themes <- list(
   # https://coolors.co/f8f6f2-2a7f62-ec4e20-3772ff-092b36
-  "solar (soft light)" = bslib::bs_theme(
+  "Natural (soft light)" = bslib::bs_theme(
     bootswatch = "solar", version = "4",
     bg = "#F8F6F2", fg = "#000000",
-    primary = "#092B36", secondary = "#2A7F62") #%>%
-    #bslib::bs_add_rules(".card { background: $secondary }") %>%
-    #bslib::bs_add_rules(".card input { background: $secondary }")
+    primary = "#092B36", secondary = "#2A7F62") %>%
+    bslib::bs_add_rules("body { background-image: linear-gradient(to bottom, #F8F6F2, #DBD1BD, #B09A6D); }")
 )
 
 
