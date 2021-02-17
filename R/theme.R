1197567d0f0c50274df6480417aee1f38ea7359d
amar00k <danielneves2005@gmail.com>
2021-02-17 12:53:22 +0000

    enh: Natural theme now based on the minty bootswatch theme.

--- R/theme.R
+++ R/theme.R
@@ -5,7 +5,7 @@
 rslate.themes <- list(
   # https://coolors.co/f8f6f2-2a7f62-ec4e20-3772ff-092b36
   "Natural (soft light)" = bslib::bs_theme(
-    bootswatch = "solar", version = "4",
+    bootswatch = "minty", version = "4",
     bg = "#F8F6F2", fg = "#000000",
     primary = "#092B36", secondary = "#2A7F62") %>%
     bslib::bs_add_rules("body { background-image: linear-gradient(to bottom, #F8F6F2, #DBD1BD, #B09A6D); }")
