9ce901579f05fb7dfef2161570ee4b3ec4d45967
amar00k <danielneves2005@gmail.com>
2021-02-18 17:59:07 +0000

    enh: Natural improved, and now defines new bg-light, bg-dark and bg-title utility classes.

--- R/theme.R
+++ R/theme.R
@@ -3,12 +3,18 @@
 
 
 rslate.themes <- list(
-  # https://coolors.co/f8f6f2-2a7f62-ec4e20-3772ff-092b36
+  # https://coolors.co/f8f6f2-2a7f62-182825-3772ff-092b36
   "Natural (soft light)" = bslib::bs_theme(
     bootswatch = "minty", version = "4",
     bg = "#F8F6F2", fg = "#000000",
-    primary = "#092B36", secondary = "#2A7F62") %>%
-    bslib::bs_add_rules("body { background-image: linear-gradient(to bottom, #F8F6F2, #DBD1BD, #B09A6D); }")
+    primary = "#092B36", secondary = "#2A7F62"
+    ) %>%
+    bslib::bs_add_variables(
+      "theme-colors" = "('light': #F8F6F2, 'dark': #182825, 'title': #26403B)"
+    ) %>%
+    bslib::bs_add_rules("
+      body { background-image: linear-gradient(
+        to bottom, #F8F6F2, #DBD1BD 1000px, #B09A6D 2000px, #D64933 100%) ; }")
 )
 
 
