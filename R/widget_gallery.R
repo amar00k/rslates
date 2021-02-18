cb50e5b801236109607fdab86e0e6a827aee1560
amar00k <danielneves2005@gmail.com>
2021-02-18 18:00:22 +0000

    enh: small fixes to the widget gallery

--- R/widget_gallery.R
+++ R/widget_gallery.R
@@ -1,21 +1,20 @@
 
 
 
-widgetGalleryUI <- function(id, input.list) {
+widgetGalleryInputsUI <- function(id, input.list) {
   ns <- NS(id)
 
-  inputs.ui <- tagList(
-    createInputGroup(inputGroup(name = "inputs", layout = "flow-3", inputs = input.list), ns = ns),
-    #hr(),
+  div(
+    class = "container",
+    createInputGroup(inputGroup(name = "inputs", layout = "flow-3", children = input.list), ns = ns),
     #reactable::reactableOutput(ns("input_table"))
-    tableOutput(ns("input_table"))
-  )
-
-  outputs.ui <- tagList()
-
-  tabsetPanel(
-    tabPanel(title = "Inputs", tags$div(class = "container p-3", inputs.ui)),
-    tabPanel(title = "Outputs", tags$div(class = "container p-3", outputs.ui))
+    div(
+      class = "card align-items-center",
+      div(
+        class = "card-body",
+        tableOutput(ns("input_table"))
+      )
+    )
   )
 }
 
@@ -25,16 +24,42 @@ widgetGalleryServer <- function(id, input.list, global.options = NULL) {
 
 
     # initialize input observers
-    for (x in input.list) {
-      input.handlers[[ x$input.type ]]$create.observer(session, x$id)
+    observe({
+      for (x in input.list) {
+        getHandler(x)$observer(x, session)
+      }
+    })
+
+
+    inputValueString <- function(x, max.length = 400) {
+      val <- getHandler(x)$get.value(x, session)
+
+      if (x$input.type == "expression" && val != "" && isValidExpression(val)) {
+        val <- eval(parse(text = val), envir = new.env())
+      }
+
+      val <- toString(val)
+      if (nchar(val) > (max.length))
+        val <- paste0(substring(val, 1, max.length / 2), "...",
+                    substring(val, nchar(val) - max.length / 2, nchar(val)))
+
+      return(val)
     }
 
 
-    inputs <- reactive({
-      lapply(input.list, function(x) {
-        x$value <- input.handlers[[ x$input.type ]]$get.value(x, session)
-        return(x)
-      }) %>% set_names(sapply(., "[[", "name"))
+    inputs.table <- reactive({
+      inputs <- input.list
+
+      data.frame(
+        Name = sapply(inputs, "[[", "name"),
+        Type = sapply(inputs, "[[", "input.type"),
+        Source = sapply(inputs, function(x) getHandler(x)$get.source(x, session)),
+        Value = sapply(inputs, function(x) inputValueString(x))
+      )
+    })
+
+    output$input_table <- renderTable({
+      inputs.table()
     })
 
 
@@ -51,50 +76,18 @@ widgetGalleryServer <- function(id, input.list, global.options = NULL) {
     #   )
     # })
 
-    objectSummary <- function(obj, max.length = 400) {
-      s <- toString(obj)
-
-      if (nchar(s) > (max.length))
-        s <- paste0(substring(s, 1, max.length / 2), "...",
-                    substring(s, nchar(s) - max.length / 2, nchar(s)))
-
-      return(s)
-    }
-
-
-    inputs.table <- reactive({
-      inputs <- inputs()
-
-      data.frame(
-        Name = sapply(inputs, "[[", "name"),
-        Type = sapply(inputs, "[[", "input.type"),
-        Value = sapply(inputs, function(x) objectSummary(x$value)),
-        Source = sapply(inputs, function(x) input.handlers[[ x$input.type ]]$get.source(x, session))
-      )
-    })
 
     # output$input_table <- reactable::renderReactable({
     #   inputs <- inputs()
     #
     #   reactable::reactable(
-    #     data.frame(
-    #       Name = sapply(inputs, "[[", "name"),
-    #       Id = sapply(inputs, "[[", "id"),
-    #       Type = sapply(inputs, "[[", "input.type"),
-    #       Value = sapply(inputs, function(x) objectSummary(x$value))
-    #     ),
+    #     inputs.table(),
     #     rownames = FALSE,
     #     theme = reactable.theme(),
     #     borderless = TRUE,
     #     sortable = FALSE
     #   )
     # })
-
-
-
-    output$input_table <- renderTable({
-      inputs.table()
-    })
 
 
   })
