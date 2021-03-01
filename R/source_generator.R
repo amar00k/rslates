83b4f223742c5a9afb4231831f237700bc00b787
amar00k <danielneves2005@gmail.com>
2021-03-01 01:29:42 +0000

    new: preprocessor code for new .txt blueprints.

--- R/source_generator.R
+++ R/source_generator.R
@@ -184,11 +184,183 @@ assignValue <- function(src, varname) {
 }
 
 
+#
+# Blueprint preprocessor
+#
+
+
+example.source <- "
+### This is a preprocessor comment. It will be suppressed in source code and
+### will not be processed by the preprocessor.
+
+### Below is a section declaration. The section contents ends when another
+### section begins or when \"#-- end section\" is read, or when the file ends.
+#-- print  ### A comment can appear inline. All further contents of line is ignored.
+
+# This is a normal R comment. It will be shown in source code.
+df <- iris
+
+#-- plot plot 1, width = 120px
+
+plot(${dn :: x:x(expression), y=y, type=type, main=main(character, \"Hello\"), xlab=xlab, ylab=ylab})
+
+#-- table my table
+
+head(iris, ${num_rows(numeric, 6, page = \"Preview Settings\")})
+
+"
 
 
 
 
+preprocessInputOptions <- function(text) {
+  stopifnot("Text must be a single string." = (length(text) == 1))
 
+  if (text == "")
+    return(list(psource = ""))
+
+  opts <- strsplit(text, split = ",")[[1]] %>% trimws
+
+  # first unnamed value is input.type
+  if (!grepl("=", opts[[1]]))
+    opts[[1]] <- paste0("input.type = '", opts[[1]], "'")
+
+  # if second unnamed value, it's the default value
+  if (length(opts) > 1 && !grepl("=", opts[[2]]))
+    opts[[2]] <- paste("default = ", opts[[2]], "")
+
+  # make a list
+  options <- new.env()
+  opts <- paste(opts, collapse = "\n")
+  eval(parse(text = opts), envir = options)
+  options <- as.list(options)
+
+  return(options)
+}
+
+
+#' Extract Input Definitions From Blueprint Source
+#'
+#' @param text a single character string representing the source code
+#'   to preprocess.
+#'
+#' @return A list where each element represents an input. Each element of
+#'   an input represents a option that can be passed on to slateInput() in
+#'   order to construct the input.
+#' @export
+#'
+#' @examples
+preprocessInputs <- function(text) {
+  # Note to self: I'm having way too much fun with purrr...
+  stopifnot("Text must be a single string." = (length(text) == 1))
+
+  # split into elements of the form ${<element>}
+  x <-
+    regmatches(text, gregexpr("\\$\\{.*?\\}", text))[[1]] %>%
+    { gsub("^\\$\\{.*?::", "", .) } %>%    # remove "${ xxx::"
+    { gsub("^\\$\\{|\\}$", "", .) } %>%    # remove  "${" and "}"
+    trimws
+
+  # get all input names
+  input.names <-
+    gsub("\\(.*?\\)", "", x) %>%
+    strsplit(split = ",") %>%
+    unlist %>%
+    { gsub(":.*$|^.*=", "", .) } %>%
+    unique
+
+  # get options for inputs that have them
+  input.options <-
+    map(x, ~regmatches(.x, gregexpr("[^ :=]+\\(.*?\\)", .x))) %>%
+    unlist %>%
+    { set_names(., gsub("\\(.*$", "", .)) } %>%
+    { gsub("^.*\\(|\\)$", "", .) } %>%
+    map(~preprocessInputOptions(.x))
+
+  # put it all together
+  inputs <-
+    map(input.names, ~c(list(name = .x), input.options[[ .x ]])) %>%
+    set_names(map(., "name")) %>%
+    modify_if(~is.null(.$input.type), ~c(.x, input.type = "character"))
+
+
+  return(inputs)
+}
+
+
+
+preprocessSection <- function(lines) {
+  section <- list()
+
+  # preprocess the header
+  if (grepl("^#--", lines[1])) {
+    # has header
+    if (length(lines) == 1) {
+      return(NULL)
+    }
+
+    x <-
+      gsub("^#--", "", lines[1]) %>%
+      { strsplit(., split = ",")[[1]] } %>%
+      trimws
+
+    section$type <- sub("^(.*?) .*$", "\\1", x[[1]])
+
+    if (grepl(" ", x[[1]]) == TRUE)
+      section$name <- sub("^.*? (.*)$", "\\1", x[[1]])
+    else
+      section$name <- "Untitled"
+
+    section$params <- if (length(x) > 1) x[[2]] else ""
+    section$source <- paste(lines[2:length(lines)], collapse="\n")
+  } else {
+    # no header: generic text output
+    section$type <- "print"
+    section$name <- "Generic"
+    section$params <- ""
+    section$source <- paste(lines, collapse = "\n")
+  }
+
+  return(section)
+}
+
+
+preprocessSections <- function(text) {
+  stopifnot("Text must be a single string." = (length(text) == 1))
+
+  # sections
+  lines <- strsplit(text, split = "\n")[[1]]
+
+  # check for section markers
+  idx <- grep("^#\\-\\-", lines)
+  if (length(idx) > 0) {
+    idx <- c(idx, length(lines)+1)
+
+    sections <-
+      map2(idx[1:(length(idx)-1)],
+           idx[ 2:length(idx) ] - 1,
+           ~lines[ .x:.y ]) %>%
+      map(preprocessSection)
+  } else {
+    sections <- list()
+  }
+
+  # check for unmarked sections
+  if (length(idx) == 0) {
+    sections <- list(preprocessSection(lines))
+  } else if (idx[1] > 1) {
+    sections <-
+      lines[1:(idx[1] - 1)] %>%
+      preprocessSection %>%
+      { c(list(.), sections) }
+  }
+
+  sections %<>%
+    discard(is.null) %>%
+    set_names(map(., "name"))
+
+  return(sections)
+}
 
 
 
