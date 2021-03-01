680ac9257a45375959d5dcec58ebc0cea122d19b
amar00k <danielneves2005@gmail.com>
2021-03-01 01:35:35 +0000

    test/wip: tests for new preprocessor functions

--- tests/testthat/test-source_generator.R
+++ tests/testthat/test-source_generator.R
@@ -8,3 +8,14 @@ inputs <- list(
 test_that("srcBuild works", {
   expect_equal(srcBuild(srcParse("paste(${x}, ${y})"), inputs), 'paste("HELLO", 42)')
 })
+
+
+test_that("extractInputs works", {
+
+  text <- "range <- ${from}:${to}"
+  expect_equal(ppExtractInputs(text)$input.names, c("from", "to"))
+
+  text <- "plot(${dn :: x:x, y=y, type=type, main=main(character, Hello), xlab=xlab, ylab=ylab})"
+  expect_equal(ppExtractInputs(text)$input.names, c("x", "y", "type", "main", "xlab", "ylab"))
+
+})
