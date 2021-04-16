aac30225f4427bba185f43b81987edcf19871fc6
amar00k <danielneves2005@gmail.com>
2021-04-16 13:16:22 +0100

    wip: new blueprint editor; these functions will eventually be moved to blueprint_editor.R

--- R/slate_builder.R
+++ R/slate_builder.R
@@ -4,6 +4,10 @@
 
 
 
+
+
+
+
 createTypeSpecificUI <- function(ns, item) {
   parseChoices <- function(choices) {
     if (!is.null(names(choices))) {
@@ -55,15 +59,15 @@ createTypeSpecificUI <- function(ns, item) {
 createItemPropertiesUI <- function(id, item) {
   ns <- NS(id)
 
-  if (item$type == "page") {
+  if (class(item) == "slatePage") {
     ui <- tagList(
-      tags$h4("Page Properties", paste0("(", item$name, ")")),
+      tags$h5("Page Properties", paste0("(", item$name, ")")),
       tags$hr(),
       textAreaInput(ns("page_description"), label = "Description", item$description)
     )
-  } else if (item$type == "group") {
+  } else if (class(item) == "slateGroup") {
     ui <- tagList(
-      tags$h4("Group Properties"),
+      tags$h5("Group Properties"),
       tags$hr(),
       selectInput(ns("group_layout"), label = "Layout",
                   choices = c("flow-2",
@@ -73,16 +77,16 @@ createItemPropertiesUI <- function(id, item) {
                   selected = item$layout),
       textInput(ns("group_condition"), label = "Condition", item$condition)
     )
-  } else if (item$type == "input") {
+  } else if (class(item) == "slateInput") {
     ui <- tagList(
-      tags$h4("Input Properties", paste0("(", item$name, ")")),
+      tags$h5("Input Properties", paste0("(", item$name, ")")),
       tags$hr(),
       tags$div(
         class = "slates-flow-2",
         selectInput(ns("input_input.type"), label = "Type",
                     selectize = TRUE,
                     choices = names(input.handlers),
-                    selected = item$input.type),
+                    selected = item$type),
         uiOutput(ns("input_default_ui"))
       ),
       createTypeSpecificUI(ns, item),
@@ -99,145 +103,148 @@ createItemPropertiesUI <- function(id, item) {
 }
 
 
-builderItemServer <- function(id, item.ini, global.options = NULL) {
+inputItemServer <- function(id, item, global.options = NULL) {
   moduleServer(id, function(input, output, session) {
-    item <- reactiveVal(item.ini)
+    ns <- session$ns
+
+    item <- reactiveVal(item)
+
     need.redraw <- reactiveVal("")  # change when we need to redraw this whole page
     redraw.default.ui <- reactiveVal("") # change to update default input (on entering page)
 
 
     createUI <- function() {
-      pprint("slate_builder.R: createUI(", isolate(item()$name), ")")
+      dlog(item())
 
       createItemPropertiesUI(id, isolate(item()))
     }
 
 
-    output$input_default_ui <- renderUI({
-      req(isolate(item <- item()))
-
-      pprint("slate_builder.R: redraw default for",
-             item$id, "( default =", paste(item$default, collapse = ", "), ")")
-
-      # listen to these events
-      redraw.default.ui()
-      input$input_input.type
-
-      item$name <- "Default Value"
-      item$value <- item$default
-      item$wizards <- NULL
-      getHandler(item)$create.ui(session$ns(item$id), item)
-    })
-
-
-    observe({
-      req(item <- item())
-
-      # check for a change in input.type. If changed:
-      # 1. set need.redraw to a new value to redraw the UI
-      # 2. re-initialize the input to fill in missing type-specific variables
-      # 3. restore the item's ancestry
-      if (item$type == "input" &&
-          !is.null(input$input_input.type) &&
-          item$input.type != input$input_input.type) {
-        need.redraw(runif(1))
-
-        ancestry <- item$ancestry
-        item <- slateInput(name = item$name, input.type = input$input_input.type,
-                           long.name = item$long.name, description = item$description,
-                           wizards = item$wizards)
-        print(item)
-        item$ancestry <- ancestry
-      }
-
-      # check for changes in general item properties
-      variables <- c("description", "layout", "condition", "description", "wizards")
-      for (var in variables) {
-        input.id <- paste0(item$type, "_", var)
-
-        if (!is.null(input[[ input.id ]]))
-          item[[ var ]] <- input[[ input.id ]]
-      }
-
-      # check for changes in input type-specific parameters
-      if (item$type == "input") {
-        params <- getHandler(item)$params.list
-
-        for (var in names(params)) {
-          par <- params[[ var ]]
-          input.id <- paste0("input_", var)
-
-          if (!is.null(input[[ input.id ]])) {
-            item[[ var ]] <- switch(
-              par$type,
-              "list" = (function(x) {
-                if (all(grepl("=", x))) {
-                  strsplit(x, split="=") %>%
-                    { setNames(lapply(., "[[", 2), sapply(., "[[", 1)) }
-                } else {
-                  as.list(x) %>% unname
-                }
-              })(input[[ input.id ]]),
-              "choices" = input[[ input.id ]],
-              "logical" = as.logical(input[[ input.id ]])
-            )
-          }
-        }
-      }
-
-      # if any type-specific parameters changed, redraw default input
-      if (item$type == "input") {
-        params <- getHandler(item)$params.list
-        if (length(params) > 0 &&
-            !all(sapply(names(params), function(var) identical(item[[ var ]], item()$var)))) {
-          print("here")
-          redraw.default.ui(runif(1))
-        }
-      }
-
-      if (!identical(item, item())) {
-        pprint("slate_builder.R: update item (", item$name, ")")
-
-        item(item)
-      }
-    })
-
-
-    # get the value of the default input, taking the input type into consideration
-    default.value <- reactive({
-      req(
-        item <- isolate(item()),
-        input$input_input.type
-      )
-
-      value <- getHandler(item)$get.value(item, session)
-
-      if (length(value) > 0) {
-        req(all(!sapply(value, is.null)))
-      } else {
-        req(!is.null(value))
-      }
-
-      return(value)
-    })
-
-
-    # check for changes in default value
-    observe({
-      req(
-        item <- isolate(item())
-      )
-
-      value <- default.value()
-
-      if (!identical(item$default, value)) {
-        pprint("Setting new default for", item$name, ":", paste(value, collapse=", "))
-
-        item$value <- item$default <- value
-
-        item(item)
-      }
-    })
+    # output$input_default_ui <- renderUI({
+    #   req(isolate(item <- item()))
+    #
+    #   pprint("slate_builder.R: redraw default for",
+    #          item$id, "( default =", paste(item$default, collapse = ", "), ")")
+    #
+    #   # listen to these events
+    #   redraw.default.ui()
+    #   input$input_input.type
+    #
+    #   item$name <- "Default Value"
+    #   item$value <- item$default
+    #   item$wizards <- NULL
+    #   getHandler(item)$create.ui(session$ns(item$id), item)
+    # })
+    #
+    #
+    # observe({
+    #   req(item <- item())
+    #
+    #   # check for a change in input.type. If changed:
+    #   # 1. set need.redraw to a new value to redraw the UI
+    #   # 2. re-initialize the input to fill in missing type-specific variables
+    #   # 3. restore the item's ancestry
+    #   if (item$type == "input" &&
+    #       !is.null(input$input_input.type) &&
+    #       item$input.type != input$input_input.type) {
+    #     need.redraw(runif(1))
+    #
+    #     ancestry <- item$ancestry
+    #     item <- slateInput(name = item$name, input.type = input$input_input.type,
+    #                        long.name = item$long.name, description = item$description,
+    #                        wizards = item$wizards)
+    #     print(item)
+    #     item$ancestry <- ancestry
+    #   }
+    #
+    #   # check for changes in general item properties
+    #   variables <- c("description", "layout", "condition", "description", "wizards")
+    #   for (var in variables) {
+    #     input.id <- paste0(item$type, "_", var)
+    #
+    #     if (!is.null(input[[ input.id ]]))
+    #       item[[ var ]] <- input[[ input.id ]]
+    #   }
+    #
+    #   # check for changes in input type-specific parameters
+    #   if (item$type == "input") {
+    #     params <- getHandler(item)$params.list
+    #
+    #     for (var in names(params)) {
+    #       par <- params[[ var ]]
+    #       input.id <- paste0("input_", var)
+    #
+    #       if (!is.null(input[[ input.id ]])) {
+    #         item[[ var ]] <- switch(
+    #           par$type,
+    #           "list" = (function(x) {
+    #             if (all(grepl("=", x))) {
+    #               strsplit(x, split="=") %>%
+    #                 { setNames(lapply(., "[[", 2), sapply(., "[[", 1)) }
+    #             } else {
+    #               as.list(x) %>% unname
+    #             }
+    #           })(input[[ input.id ]]),
+    #           "choices" = input[[ input.id ]],
+    #           "logical" = as.logical(input[[ input.id ]])
+    #         )
+    #       }
+    #     }
+    #   }
+    #
+    #   # if any type-specific parameters changed, redraw default input
+    #   if (item$type == "input") {
+    #     params <- getHandler(item)$params.list
+    #     if (length(params) > 0 &&
+    #         !all(sapply(names(params), function(var) identical(item[[ var ]], item()$var)))) {
+    #       print("here")
+    #       redraw.default.ui(runif(1))
+    #     }
+    #   }
+    #
+    #   if (!identical(item, item())) {
+    #     pprint("slate_builder.R: update item (", item$name, ")")
+    #
+    #     item(item)
+    #   }
+    # })
+    #
+    #
+    # # get the value of the default input, taking the input type into consideration
+    # default.value <- reactive({
+    #   req(
+    #     item <- isolate(item()),
+    #     input$input_input.type
+    #   )
+    #
+    #   value <- getHandler(item)$get.value(item, session)
+    #
+    #   if (length(value) > 0) {
+    #     req(all(!sapply(value, is.null)))
+    #   } else {
+    #     req(!is.null(value))
+    #   }
+    #
+    #   return(value)
+    # })
+    #
+    #
+    # # check for changes in default value
+    # observe({
+    #   req(
+    #     item <- isolate(item())
+    #   )
+    #
+    #   value <- default.value()
+    #
+    #   if (!identical(item$default, value)) {
+    #     pprint("Setting new default for", item$name, ":", paste(value, collapse=", "))
+    #
+    #     item$value <- item$default <- value
+    #
+    #     item(item)
+    #   }
+    # })
 
     list(
       item = item,
